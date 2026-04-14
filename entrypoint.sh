4cc9671b0e6a1120815324e6b917aea02aea6691
callmetango <callmetango@users.noreply.github.com>
2026-04-14 00:59:31 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,19 +11,19 @@ log_endgroup() {
 }
 
 HOME=/home/builder
+WORKPATH=$GITHUB_WORKSPACE/$INPUT_PATH
+WORKPATH=${WORKPATH%/}
 
-log_group "Copying files from $GITHUB_WORKSPACE to $HOME/gh-action"
-mkdir -p "$HOME"/gh-action
-cd "$HOME"/gh-action
+log_group "Copying files from $GITHUB_WORKSPACE to $HOME/work"
+mkdir -p "$HOME"/work
+cd "$HOME"/work
+cp -rfv "$WORKPATH"/ ./
 log_endgroup
 
 log_group "Running repo_add"
 repo_add $INPUT_FLAGS $INPUT_REPO_NAME *.pkg.*
 log_endgroup
 
-WORKPATH=$GITHUB_WORKSPACE
-WORKPATH=${WORKPATH%/}
-
-log_group "Copying files from $HOME/gh-action to $WORKPATH"
-sudo cp -fv * "$WORKPATH"
+log_group "Copying files from $HOME/work to $WORKPATH"
+sudo cp -fvu * "$WORKPATH"
 log_endgroup
